03d62ca88ef0bd7de2154df954c41a804f552d50
Yucheng Chuang <ducksteven@gmail.com>
2018-11-21 22:22:28 +0900

    Remove tools I don't use for long time

--- Brewfile.personal.rb
+++ Brewfile.personal.rb
@@ -6,22 +6,14 @@ tap 'caskroom/versions'
 
 brew 'mas'
 
-cask 'google-chrome-beta'
+cask 'google-chrome'
 cask 'iina'
 cask 'istat-menus'
 cask 'macvim'
-cask 'mplayerx'
-cask 'skype'
-cask 'spotify'
-cask 'vlc'
 
 mas 'Debit & Credit', id: 882637653
-mas 'ForkLift', id: 412448059
 
 mas 'The Unarchiver', id: 425424353
-
-mas 'LINE', id: 539883307
-mas 'Telegram', id: 747648890
 
 mas 'Pages', id: 409201541
 mas 'Numbers', id: 409203825
