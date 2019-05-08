31eb3e346a685a35a9f3f1f5f0313ba1fabdc1d4
Yucheng Chuang <ychuang@moneytree.jp>
2019-05-08 10:34:36 +0900

    Adjust Hyper.js config

--- rc/apps/.hyper.js
+++ rc/apps/.hyper.js
@@ -5,10 +5,10 @@
 module.exports = {
   config: {
     // default font size in pixels for all tabs
-    fontSize: 16,
+    fontSize: 14,
 
     // font family with optional fallbacks
-    fontFamily: 'Fira Code, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: 'SFMono-Light, Fira Code, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: 'rgba(248,28,229,0.8)',
@@ -104,17 +104,18 @@ module.exports = {
   //   `hyperpower`
   //   `@company/project`
   //   `project#1.0.1`
-  plugins: [
-    "hyper-dracula",
-    // 'hyper-material-theme',
-    // 'hyperterm-safepaste',
-    'hyperlinks'
-    // 'hyper-tabs-enhanced',
-    // 'hyper-statusline'
-  ],
+  plugins: ["hyper-dracula"],
 
   // in development, you can create a directory under
   // `~/.hyper_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
-  localPlugins: []
+  localPlugins: [],
+
+  hyperline: {
+    plugins: [
+      "cpu",
+      "memory",
+      "network"
+    ]
+  }
 };
