f0906a184d7f98ad009277be52a8f9972cd1a2b6
Yucheng Chuang <ducksteven@gmail.com>
2026-02-21 11:18:40 +0900

    quality of life on macos

--- macos.sh
+++ macos.sh
@@ -7,8 +7,9 @@ set -euxo pipefail
 # Enable Secure Keyboard Entry in Terminal.app
 defaults write com.apple.terminal SecureKeyboardEntry -bool true
 
-# Press & hold = entering same character
+# Press & hold = repeat the same character
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+defaults write dev.zed.Zed ApplePressAndHoldEnabled -bool false
 
 # Enable full keyboard access for all controls
 # (e.g. enable Tab in modal dialogs)
@@ -18,11 +19,16 @@ defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 defaults write NSGlobalDomain KeyRepeat -int 2
 defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
+# Disable double space = period
+# See https://github.com/AdamMaras/vscode-overtype/issues/9#issuecomment-1032198855
+defaults write NSGlobalDomain NSAutomaticPeriodSubstitutionEnabled -bool false
+
 # Trackpad: enable tap to click
 defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -int 1
 defaults write com.apple.AppleMultitouchTrackpad Clicking -int 1
 defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
 
-# Enforce best sound quality for Bluetooth headphones.
-# See https://www.macrumors.com/how-to/enable-aptx-aac-bluetooth-audio-codecs-macos/
-sudo defaults write bluetoothaudiod "Enable AptX codec" -bool true
+# Disable menu icons on Tahoe
+# See https://lobste.rs/s/7xegog/removing_tahoe_s_unwanted_menu_icons#c_mutg0j
+defaults write -globalDomain NSMenuEnableActionImages -bool NO
+
